5cf0970b0dfb8818efdcf60b407227dddf6a8720
MVP\arnie.chipe <akchipe@gmail.com>
2018-09-24 17:18:22 +0800

    mysql Conntection credentials
    Connection String

--- MovieListExam/settings.py
+++ MovieListExam/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'MovieListExam.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'movielist',
+        'USER': 'root',
+        'PASSWORD': 'qwerty',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
